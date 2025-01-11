03fb16c6d8d64293467422d4d4baffd953eed927
Konstantin Alaev <alaevdev@gmail.com>
2025-01-11 23:55:24 +0300

    feat: fix brew formula

--- Formula/erun.rb
+++ Formula/erun.rb
@@ -1,8 +1,8 @@
 class Erun < Formula
   desc "CLI tool to launch iOS simulators and Android emulators"
   homepage "https://github.com/alaev-dev/erun"
-  url "https://github.com/alaev-dev/erun/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "2dc9fbe707051d76bfb926c61bddb0f130418b3eede3e787b885ecc8a5f7bd59"
+  url "https://github.com/alaev-dev/erun/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "9fafec0ff1be81d72a4eae5e0f040b6d809b568732f6950d628cbac3fc4b81e7"
   license "MIT"
   depends_on "dart-sdk"
 
