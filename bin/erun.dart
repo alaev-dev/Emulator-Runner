02368bc34e00732803ed27609b83105d927a04c8
Konstantin Alaev <alaevdev@gmail.com>
2025-01-11 15:28:16 +0300

    Init

---
+++ bin/erun.dart
@@ -0,0 +1,5 @@
+import 'package:erun/erun.dart' as erun;
+
+void main(List<String> arguments) {
+  print('Hello world: ${erun.calculate()}!');
+}
---
+++ test/erun_test.dart
@@ -0,0 +1,8 @@
+import 'package:erun/erun.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
